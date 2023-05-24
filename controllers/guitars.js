22b42d8ae7d4d2cb8f177e787ecb6ad2a862fc75
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-24 13:22:11 -0400

    stub up all controller functions

--- controllers/guitars.js
+++ controllers/guitars.js
@@ -1,6 +1,44 @@
 const { Guitar } = require('../models')
 
+const create = async (req, res) => {
+  try {
+    const guitar = await Guitar.create(req.body)
+    res.status(200).json(guitar)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const index = async (req, res) => {
+  try {
+    const guitar = await Guitar.create(req.body)
+    res.status(200).json(guitar)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const guitar = await Guitar.create(req.body)
+    res.status(200).json(guitar)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteGuitar = async (req, res) => {
+  try {
+    const guitar = await Guitar.create(req.body)
+    res.status(200).json(guitar)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 module.exports = {
-
+  create,
+  index,
+  update,
+  delete: deleteGuitar,
 }
